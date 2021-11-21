02ad7f0d502b0092ea69cfc8389874797a4aa6d4
zkl2333 <i@zkl2333.com>
2021-11-21 13:18:05 +0800

    feat: 🎸 优化首页

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  purge: [],
-  darkMode: 'media', // false or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	mode: "jit",
+	purge: ["./**/*.{js,jsx,ts,tsx}"],
+	darkMode: false, // false or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
