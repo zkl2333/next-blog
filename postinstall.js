1dcd98a78855ff214edc8c00b2aebc91e3609df8
zkl2333 <i@zkl2333.com>
2021-12-04 08:20:33 +0800

    ci: 🎡 部署脚本

--- postinstall.js
+++ postinstall.js
@@ -4,5 +4,5 @@ console.log(process.env)
 
 // if (process.env.LEANCLOUD_APP_ID) {
     console.log('leancloud 部署中...')
-    child.execSync('next build', { stdio: 'inherit' })
+    child.execSync('npx next build', { stdio: 'inherit' })
 // }
